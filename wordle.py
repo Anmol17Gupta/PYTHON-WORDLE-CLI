e846912118f7185fdae6fb7cc6a5b572d56df318
Anmol Gupta <118355231+Anmol17Gupta@users.noreply.github.com>
2026-06-30 23:35:14 +0530

    feat: add CLI gameplay with colored Wordle board

--- wordle.py
+++ wordle.py
@@ -3,6 +3,7 @@ class Wordle:
 
     MAX_ATTEMPTS = 6
     WORD_LENGTH = 5
+    VOIDED_LETTER = "*"
 
     def __init__(self, secret: str):
         self.secret: str = secret.upper()
@@ -14,15 +15,36 @@ class Wordle:
     
     def guess(self, word: str):
         word = word.upper()
-        result = []
+
+        # Initialise the results array with all GREY letters
+        result = [LetterState(x) for x in word]
+
+        # Make a copy of the secret so we can cross out "used" letters
+        remaining_secret = list(self.secret)
+
+        #First, check for GREEN letters.
         for i in range(self.WORD_LENGTH):
-            character = word[i]
-            letter = LetterState(character)
-            letter.is_in_word = character in self.secret # liner operation since word length is 5 we can use that 
-            letter.is_in_position = character == self.secret[i]
-            result.append(letter)
-        
-        return result
+            letter = result[i]
+            if letter.character == remaining_secret[i] :
+                letter.is_in_position = True
+                remaining_secret[i] = self.VOIDED_LETTER
+
+        # Loop again and check for YELLOW letters.
+        for i in range(self.WORD_LENGTH):
+            letter = result[i]
+
+            # Skip this letter if it is already in the right place
+            if letter.is_in_position:
+                continue
+            
+            # Otherwise, check if the letter is in the word and void that index
+            for j in range(self.WORD_LENGTH):
+                if letter.character == remaining_secret[j]:
+                    remaining_secret[j] = self.VOIDED_LETTER
+                    letter.is_in_word = True
+                    break
+
+        return result 
     
     @property
     def is_solved(self):
